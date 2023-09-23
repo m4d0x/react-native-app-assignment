4f4d6a8460589afb9649a9b706f997f74e97dbfe
m4d0x <robert.oxhjarta@hotmail.com>
2023-09-23 21:30:46 +0200

    Init tabbar navigation with icons

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
+import { Fontisto } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <Text>Home Screen</Text>
       <StatusBar style='auto' />
     </View>
@@ -12,19 +17,64 @@ function HomeScreen() {
 
 function DetailsScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Details Screen</Text>
       <StatusBar style='auto' />
     </View>
   );
 }
 
-export default function App() {
+function SettingsScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Settings Screen</Text>
       <StatusBar style='auto' />
     </View>
+  );
+}
+
+const Tabs = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar style='auto' />
+        <Tabs.Navigator>
+          <Tabs.Screen
+            name='Home'
+            component={HomeScreen}
+            options={{
+              tabBarIcon: (props) => (
+                <Fontisto name='home' size={props.size} color='black' />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name='Details'
+            component={DetailsScreen}
+            options={{
+              tabBarIcon: (props) => (
+                <Fontisto name='search' size={props.size} color='black' />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name='Settings'
+            component={SettingsScreen}
+            options={{
+              tabBarIcon: (props) => (
+                <Fontisto
+                  name='player-settings'
+                  size={props.size}
+                  color='black'
+                />
+              ),
+            }}
+          />
+        </Tabs.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
