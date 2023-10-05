d52d0a8e7ee91087590ddb8920124120ee1e2766
m4d0x <robert.oxhjarta@hotmail.com>
2023-10-05 17:43:57 +0200

    Return init template but not correct yet

--- src/components/SecretCard.tsx
+++ src/components/SecretCard.tsx
@@ -1,6 +1,6 @@
 //SecretCard.tsx
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { useTheme } from '../contexts/ThemeContext';
 import ThemedLikeButton from './ThemedLikeButton';
@@ -15,3 +15,33 @@ type Secret = {
 type SecretCardProps = {
   secret: Secret;
 };
+
+export default function SecretCard({ secret }: SecretCardProps) {
+    const { theme } = useTheme();
+    return (
+        <View style={[styles.card, { backgroundColor: theme.cardBackground }]}>
+            <View style={styles.cardContent}>
+                <View style={styles.textContainer}>
+                    <Text style={{ color: theme.cardText }}>
+                        {secret.author} wrote:
+                    </Text>
+                    <Text style={{ color: theme.cardText }}>{secret.text}</Text>
+                    <Text style={{ color: theme.cardText }}>{secret.timestamp}</Text>
+                </View>
+                <View style={styles.iconContainer}>
+                    <ThemedLikeButton onPress={() => {}} />
+                    <AntDesign
+                        name="dislike1"
+                        size={24}
+                        color={theme.tabBarActiveTintColor}
+                        style={styles.iconSpacing}
+                    />
+                    <AntDesign
+                        name="heart"
+                        size={24}
+                        color={theme.tabBarActiveTintColor}
+                        style={styles.iconSpacing}
+                    />
+                </View>
+            </View>
+    )
