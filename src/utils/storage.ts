886525bc136efd0c7ab556aba5bf04fbd3ac1a82
m4d0x <robert.oxhjarta@hotmail.com>
2023-10-05 18:00:48 +0200

    toggLike-logic added, will test when possible

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -10,6 +10,7 @@ type Secret = {
   likes: number; // Nytt attribut för att hålla reda på antalet likes som getts till en secret
 };
 
+//Funktion för att lagra en hemlighet
 export const storeData = async (value: string) => {
   try {
     // Först, hämta den befintliga arrayen av hemligheter
@@ -50,4 +51,25 @@ export const getData = async () => {
   }
 };
 
-export const toggleLike = async (id: string) => {};
+export const toggleLike = async (id: string) => {
+  try {
+    //1. Hämta den befintliga arrayen av hemligheter
+    const existingData: Secret[] | null = await getData();
+
+    if (!existingData) return;
+
+    //2. Hitta rätt Secret och uppdatera dess likes
+    const updatedData = existingData.map((secret) => {
+      if (secret.id === id) {
+        return { ...secret, likes: secret.likes + 1 }; // Ökar antalet likes med 1
+      }
+      return secret;
+    });
+
+    //3. Omvandla till JSON och lagra
+    const jsonValue = JSON.stringify(updatedData);
+    await AsyncStorage.setItem('@secret_key', jsonValue);
+  } catch (e) {
+    // Om Error
+  }
+};
