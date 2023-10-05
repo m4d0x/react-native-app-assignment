d56516cdfa96d8908e034b8db267ab37fd0dcca3
m4d0x <robert.oxhjarta@hotmail.com>
2023-10-05 17:40:43 +0200

    Imports added

---
+++ src/components/SecretCard.tsx
@@ -0,0 +1,6 @@
+//SecretCard.tsx
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { useTheme } from '../contexts/ThemeContext';
+import ThemedLikeButton from './ThemedLikeButton';
