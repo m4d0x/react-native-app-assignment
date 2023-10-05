74bdee8b0476f6d0ecfa78575a78b18040426821
m4d0x <robert.oxhjarta@hotmail.com>
2023-10-05 17:21:42 +0200

    return added

--- src/components/ThemedButton.tsx
+++ src/components/ThemedButton.tsx
@@ -1,5 +1,6 @@
-import { StyleProp, ViewStyle } from 'react-native';
+import { StyleProp, TouchableOpacity, ViewStyle } from 'react-native';
 import { useTheme } from 'react-native-elements';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import { useToggle } from '../hooks/useToggle';
 
 interface ThemedLikeButtonProps {
@@ -9,7 +10,29 @@ interface ThemedLikeButtonProps {
   style?: StyleProp<ViewStyle>;
 }
 
-const ThemedLikeButton: React.FC<ThemedLikeButtonProps> = ({ onPress, style }) => {
+const ThemedLikeButton: React.FC<ThemedLikeButtonProps> = ({
+  onPress,
+  style,
+}) => {
   const theme = useTheme();
-  const [isLiked, toggleLike] = useToggle(false, (currentState) => !currentState, 'likeKey');
+  const [isLiked, toggleLike] = useToggle(
+    false,
+    (currentState) => !currentState,
+    'likeKey',
+  );
 
+  const handlePress = () => {
+    toggleLike();
+    onPress();
+  };
+
+  return (
+    <TouchableOpacity onPress={handlePress} style={style}>
+      <AntDesign
+        name={isLiked ? 'heart' : 'hearto'}
+        size={24}
+        color={isLiked ? theme.toggleActiveColor : theme.toggleInactiveColor}
+      />
+    </TouchableOpacity>
+  );
+};
