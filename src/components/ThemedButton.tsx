e74434ed5014f243ed14f52c4889a8dce3527121
m4d0x <robert.oxhjarta@hotmail.com>
2023-10-05 17:20:15 +0200

    type expanded and some of the Button logic init

--- src/components/ThemedButton.tsx
+++ src/components/ThemedButton.tsx
@@ -1,5 +1,15 @@
-interface THemedLikeButtonProps {
+import { StyleProp, ViewStyle } from 'react-native';
+import { useTheme } from 'react-native-elements';
+import { useToggle } from '../hooks/useToggle';
+
+interface ThemedLikeButtonProps {
   theme: string;
   like: boolean;
-  onClick: () => void;
+  onPress: () => void;
+  style?: StyleProp<ViewStyle>;
 }
+
+const ThemedLikeButton: React.FC<ThemedLikeButtonProps> = ({ onPress, style }) => {
+  const theme = useTheme();
+  const [isLiked, toggleLike] = useToggle(false, (currentState) => !currentState, 'likeKey');
+
