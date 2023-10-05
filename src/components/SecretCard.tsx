9ba09123b9d497e779d3af2f590d1ea7db0b48b3
m4d0x <robert.oxhjarta@hotmail.com>
2023-10-05 17:41:36 +0200

    SecretCard temporary typed before additions

--- src/components/SecretCard.tsx
+++ src/components/SecretCard.tsx
@@ -4,3 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { useTheme } from '../contexts/ThemeContext';
 import ThemedLikeButton from './ThemedLikeButton';
+
+type Secret = {
+  id: string;
+  author: string;
+  text: string;
+  timestamp: string;
+};
