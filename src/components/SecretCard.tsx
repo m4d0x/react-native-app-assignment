4a4154c525f544fea3a02dd186e8cf5ecc849658
m4d0x <robert.oxhjarta@hotmail.com>
2023-10-05 17:45:14 +0200

    Return somewhat better atm

--- src/components/SecretCard.tsx
+++ src/components/SecretCard.tsx
@@ -1,9 +1,9 @@
 //SecretCard.tsx
 import React from 'react';
 import { Text, View } from 'react-native';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import { useTheme } from '../contexts/ThemeContext';
+
 import ThemedLikeButton from './ThemedLikeButton';
+import { useTheme } from '../contexts/ThemeContext';
 
 type Secret = {
   id: string;
@@ -17,31 +17,20 @@ type SecretCardProps = {
 };
 
 export default function SecretCard({ secret }: SecretCardProps) {
-    const { theme } = useTheme();
-    return (
-        <View style={[styles.card, { backgroundColor: theme.cardBackground }]}>
-            <View style={styles.cardContent}>
-                <View style={styles.textContainer}>
-                    <Text style={{ color: theme.cardText }}>
-                        {secret.author} wrote:
-                    </Text>
-                    <Text style={{ color: theme.cardText }}>{secret.text}</Text>
-                    <Text style={{ color: theme.cardText }}>{secret.timestamp}</Text>
-                </View>
-                <View style={styles.iconContainer}>
-                    <ThemedLikeButton onPress={() => {}} />
-                    <AntDesign
-                        name="dislike1"
-                        size={24}
-                        color={theme.tabBarActiveTintColor}
-                        style={styles.iconSpacing}
-                    />
-                    <AntDesign
-                        name="heart"
-                        size={24}
-                        color={theme.tabBarActiveTintColor}
-                        style={styles.iconSpacing}
-                    />
-                </View>
-            </View>
-    )
+  const { theme } = useTheme();
+  return (
+    <View style={[styles.card, { backgroundColor: theme.cardBackground }]}>
+      <View style={styles.cardContent}>
+        <View style={styles.textContainer}>
+          <Text style={{ color: theme.cardText }}>{secret.author} wrote:</Text>
+          <Text style={{ color: theme.cardText }}>{secret.text}</Text>
+          <Text style={{ color: theme.cardText }}>{secret.timestamp}</Text>
+        </View>
+        <View style={styles.iconContainer}>
+          <ThemedLikeButton />
+          {/* Här kan du lägga till fler ikoner om du vill */}
+        </View>
+      </View>
+    </View>
+  );
+}
